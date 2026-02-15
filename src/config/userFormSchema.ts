a0ca7197b2e9a5620a76dbb371b37d55670933aa
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-15 13:13:41 +0530

    setup done

---
+++ src/config/userFormSchema.ts
@@ -0,0 +1,6 @@
+export const userFields = [
+  { name: "firstName", label: "First Name", type: "text", required: true },
+  { name: "lastName", label: "Last Name", type: "text", required: true },
+  { name: "phone", label: "Phone Number", type: "text", required: true },
+  { name: "email", label: "Email Address", type: "email", required: true },
+];
